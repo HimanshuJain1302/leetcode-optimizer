2060c9e085f36b8850c09bcf4a8a894128f04b74
Himanshu <himanshujain1280@gmail.com>
2025-03-26 19:04:07 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+import google.generativeai as genai
+
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model=genai.GenerativeModel("gemini-2.0-flash") 
+def get_gemini_response(question):
+    
+    response=model.generate_content(question)
+    return response.text
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Chatbot")
+
+st.header("Learning Project")
+
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask")
+
+if submit:
+    response=get_gemini_response(input)
+    st.subheader("The answer is")
+    st.write(response)
